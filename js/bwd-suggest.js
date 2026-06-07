37d7a776a78943d0bf561ed8b611efb0bcc4b98d
krudans <krudans@gmail.com>
2026-06-07 22:32:11 +0900

    Add files via upload

--- js/bwd-suggest.js
+++ js/bwd-suggest.js
@@ -5,7 +5,7 @@
   if (typeof BWD_DICT === 'undefined' && !document.getElementById('bwd-dict-loader')) {
     var s = document.createElement('script');
     s.id = 'bwd-dict-loader';
-    s.src = 'js/bwd-dict.js?v=2';
+    s.src = 'js/bwd-dict.js?v=3';
     document.head.appendChild(s);
   }
 })();
